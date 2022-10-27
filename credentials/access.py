f92ed7ecc0b9f96a59cdd1e129c0585723d891ec
garbaczmaciej <furn20102@gmail.com>
2022-10-27 18:38:56 +0200

    First Commit

---
+++ credentials/access.py
@@ -0,0 +1,9 @@
+
+username = input("Enter username: ")
+password = input("Enter password: ")
+
+if username == "kondziu" and password == "91847bfghru98geguy1f049":
+    print("PERMISSION GRANTED")
+
+else:
+    print("INCORRECT CREDENTIALS")
